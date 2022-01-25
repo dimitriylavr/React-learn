3ae06020a186c3a80175d02f973cdb79da4fe242
Dmitry <ldimka@gmail.com>
2022-01-25 16:48:17 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import '../src/styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'JavaScript - это язык программирования!' },
+    { id: 2, title: 'JavaScript -2', body: 'JavaScript - это язык программирования!' },
+    { id: 3, title: 'JavaScript-3', body: 'JavaScript - это язык программирования!' },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="Название поста"/>
+        <input type="text" placeholder="Описание поста"/>
+        <button>Добавить</button>
+      </form>
+
+      <PostList posts={posts} title="Список постов!" />
     </div>
   );
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PostItem from "./PostItem";
+
+const PostList = ({posts, title}) => {
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>{title}</h1>
+            {posts.map(
+                posts =>
+                    <PostItem post={posts} key={posts.id} />
+            )}
+        </div>
+    );
+};
+
+export default PostList
